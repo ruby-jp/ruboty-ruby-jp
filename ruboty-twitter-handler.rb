b71d1e79931efef0261da932f19da2b013d976b2
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-09-08 15:04:31 +0900

    Remove access token because of unnecessary

--- ruboty-twitter-handler.rb
+++ ruboty-twitter-handler.rb
@@ -44,8 +44,6 @@ module Ruboty
         @client ||= ::Twitter::REST::Client.new do |config|
           config.consumer_key        = ENV.fetch "TWITTER_CONSUMER_KEY"
           config.consumer_secret     = ENV.fetch "TWITTER_CONSUMER_SECRET"
-          config.access_token        = ENV.fetch "TWITTER_ACCESS_TOKEN"
-          config.access_token_secret = ENV.fetch "TWITTER_ACCESS_TOKEN_SECRET"
         end
       end
 
