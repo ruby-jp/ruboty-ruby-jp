5ae727c956aa4977288e5a4e413abae04d5f852e
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-10-15 18:06:59 +0900

    Simplify ruboty-tenki

--- ruboty-tenki.rb
+++ ruboty-tenki.rb
@@ -7,14 +7,8 @@ module Ruboty
          description: "Show wttr.in",
       )
 
-      LOCATIONS = {
-        '東京': 'tokyo',
-        '大阪': 'osaka',
-      }
-
       def tenki(message)
         location = message.match_data['location']
-        location = LOCATIONS[location] || location
 
         message.reply "https://wttr.in/#{location}?lang=ja"
       end
