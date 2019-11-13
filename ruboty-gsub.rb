d36201e345333b0faf0e608086ffd0f3c77b4f8f
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-11-13 16:25:52 +0900

    Fix ruboty gsub

--- ruboty-gsub.rb
+++ ruboty-gsub.rb
@@ -1,17 +1,19 @@
 module Ruboty
   module Handlers
-    class Kawa < Base
+    class Gsub < Base
       on(
-        %r!gsub ['"](?<receiver>\S+)['"] /(?<pattern>\S+)/ ['"](?<replacement>\S+)['"]!i,
+        %r!gsub (?<receiver>['"].+['"]) /(?<pattern>.+)/ (?<replacement>['"].+['"])!im,
          name: 'gsub',
          description: '$1.gsub(Regexp.new($2), $3)',
       )
 
       def gsub(message)
         m = message.match_data
+        recv = m['receiver'].undump
         pattern = Regexp.new(m['pattern'])
+        rep = m['replacement'].undump
 
-        message.reply m['receiver'].gsub(pattern, m['replacement'])
+        message.reply recv.gsub(pattern, rep)
       end
     end
   end
