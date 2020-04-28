ee11a61e08e28d86c6440851083f48977f0aeee7
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-04-28 21:59:58 +0900

    Ruboty 配下に置いていたハンドラ以外のクラス/モジュールを Ruboty のネームスペース外に移動
    ### 概要
    元々 Ruboty 配下に全てのクラスを置くことが暗黙の規約としてあると思って Ruboty 配下にクラス/モジュールを作っていた
    そのような規約はなく、更に今回追加したハンドラ以外のクラス/モジュールは Ruboty とは独立したものであったため、 Ruboty のネームスペースの外に今回追加したクラス/モジュールを移動した
    Cache は RubyJP の関心であると考え、RubyJP 配下に置いた
    
    refs https://github.com/pocke/ruboty-ruby-jp/pull/37#discussion_r397087884

--- ruboty-channel-gacha.rb
+++ ruboty-channel-gacha.rb
@@ -16,44 +16,46 @@ module Ruboty
                                      .then { Replies::ChannelGacha.create(_1, option.pre_message) }
       end
     end
+  end
+end
 
-    module Replies
-      module ChannelGacha
-        class << self
-          def create(channels, pre_message)
-            messages(channels, pre_message).join("\n")
-          end
-
-          private
-
-          def messages(channels, pre_message)
-            [pre_message, channels.sample.channel_information].compact
-          end
-        end
+module Replies
+  module ChannelGacha
+    class << self
+      def create(channels, pre_message)
+        messages(channels, pre_message).join("\n")
       end
-    end
 
-    module Options
-      class Channel
-        def initialize(message)
-          @option = message.match_data['option']
-        end
+      private
 
-        def reload?
-          @option == '-r'
-        end
-
-        def pre_message
-          @option.split('=', 2)[1] if with_pre_message?
-        end
-
-        def with_pre_message?
-          @option =~ /\A-pre/
-        end
+      def messages(channels, pre_message)
+        [pre_message, channels.sample.channel_information].compact
       end
     end
   end
+end
 
+module Options
+  class Channel
+    def initialize(message)
+      @option = message.match_data['option']
+    end
+
+    def reload?
+      @option == '-r'
+    end
+
+    def pre_message
+      @option.split('=', 2)[1] if with_pre_message?
+    end
+
+    def with_pre_message?
+      @option =~ /\A-pre/
+    end
+  end
+end
+
+module RubyJP
   class Cache
     @store ||= ActiveSupport::Cache::MemoryStore.new
 
@@ -68,71 +70,69 @@ module Ruboty
     end
   end
 
-  module RubyJP
-    class Channel
-      attr_writer :id, :topic, :purpose
+  class Channel
+    attr_writer :id, :topic, :purpose
 
-      def initialize(channel)
-        @id = channel['id']
-        @topic = channel['topic']['value']
-        @purpose = channel['purpose']['value']
+    def initialize(channel)
+      @id = channel['id']
+      @topic = channel['topic']['value']
+      @purpose = channel['purpose']['value']
+    end
+
+    class << self
+      def cache(reload: false)
+        Cache.fetch('channels', reload: reload) { all }
       end
 
-      class << self
-        def cache(reload: false)
-          Cache.fetch('channels', reload: reload) { all }
-        end
-
-        def all
-          Ruboty::SlackApi::Channel.all_public_channels.map(&method(:new))
-        end
-      end
-
-      def channel_information
-        [channel_name, topic, purpose].compact.join("\n")
-      end
-
-      def channel_name
-        "チャンネル名: <##{@id}>"
-      end
-
-      def topic
-        "トピック: #{@topic}" if @topic.present?
-      end
-
-      def purpose
-        "説明: #{@purpose}" if @purpose.present?
+      def all
+        SlackApi::Channel.all_public_channels.map(&method(:new))
       end
     end
+
+    def channel_information
+      [channel_name, topic, purpose].compact.join("\n")
+    end
+
+    def channel_name
+      "チャンネル名: <##{@id}>"
+    end
+
+    def topic
+      "トピック: #{@topic}" if @topic.present?
+    end
+
+    def purpose
+      "説明: #{@purpose}" if @purpose.present?
+    end
   end
+end
 
-  module SlackApi
-    class Channel
-      class << self
-        def all_public_channels
-          new.fetch_public_channels
-        end
+module SlackApi
+  class Channel
+    class << self
+      def all_public_channels
+        new.fetch_public_channels
       end
+    end
 
-      def fetch_public_channels
-        channels, next_cursor = [], nil
-        until next_cursor&.empty?
-          response = request_params(next_cursor).then(&client.method(:conversations_list))
-          next_cursor = response['response_metadata']['next_cursor']
-          channels.concat(response['channels'])
-        end
-        channels
+    def fetch_public_channels
+      channels, next_cursor = [], nil
+      until next_cursor&.empty?
+        response = request_params(next_cursor).then(&client.method(:conversations_list))
+        next_cursor = response['response_metadata']['next_cursor']
+        channels.concat(response['channels'])
       end
+      channels
+    end
 
-      private
+    private
 
-      def client
-        Slack::Client.new(token: ENV.fetch('SLACK_TOKEN'))
-      end
+    def client
+      Slack::Client.new(token: ENV.fetch('SLACK_TOKEN'))
+    end
 
-      def request_params(next_cursor)
-        { exclude_archived: true, limit: 200 }.tap { _1.merge!({ cursor: next_cursor }) if next_cursor.present? }
-      end
+    def request_params(next_cursor)
+      { exclude_archived: true, limit: 200 }.tap { _1.merge!({ cursor: next_cursor }) if next_cursor.present? }
     end
   end
 end
