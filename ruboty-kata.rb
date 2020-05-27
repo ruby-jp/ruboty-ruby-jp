bbbe723008c9c095398be1c30546acc241195935
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-05-27 21:09:17 +0900

    Load standard libraries in ruboty kata

--- ruboty-kata.rb
+++ ruboty-kata.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'pathname'
 
 module Ruboty
   module Handlers
@@ -14,12 +15,23 @@ module Ruboty
         m = message.match_data
         
         kind = m[:kind] == '#' ? '--instance' : '--singleton'
-        stdout, _status = Open3.capture2e('rbs', 'method', kind, m[:klass], m[:method])
+        stdout, _status = Open3.capture2e('rbs', *requires, 'method', kind, m[:klass], m[:method])
         message.reply <<~MARKDOWN
           ```
           #{stdout.chomp}
           ```
         MARKDOWN
+      end
+
+      private def requires
+        @requires ||= libraries.flat_map { |lib| ['-r', lib] }
+      end
+
+      private def libraries
+        Pathname(`gem which rbs`.chomp)
+          .join('../../stdlib/')
+          .glob('*')
+          .filter_map { |path| path.basename.to_s unless path.to_s.end_with?('/builtin') }
       end
     end
   end
