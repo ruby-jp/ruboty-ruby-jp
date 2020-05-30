e564487cb2a19af08f23c654eb6c98cbb6c649fd
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-05-31 01:21:52 +0900

    Generate 16x16 number place

--- ruboty-numberplace.rb
+++ ruboty-numberplace.rb
@@ -4,6 +4,8 @@ module Ruboty
   module Handlers
     class NumberPlace < Base
       class UserVisibleError < StandardError; end
+      class InvalidBoard < StandardError; end
+      class TooManyTries < StandardError; end
 
       on(
          /((?:number[-_]?place)|ナンプレ|なんぷれ)(?:\s+(?<n>\d+))?/i,
@@ -20,7 +22,7 @@ module Ruboty
         message.reply ex.message
       end
 
-      def gen(n:, root_n:, box_count:, initial_filled_counts:)
+      private def gen(n:, root_n:, box_count:, initial_filled_counts:, assume_retry_count:)
         loop do
           boxes = box_count.times.map{nil}
 
@@ -45,20 +47,55 @@ module Ruboty
           end
 
           board = Namero::Board.load_from_array(boxes, n)
-          Namero::Solver.new(board, extensions: [Namero::SolverExtensions::CandidateExistsOnlyOnePlace]).solve
+          solver(board).solve
+          board, boxes = assume_and_solve(board: board, boxes: boxes) unless board.complete?
           return boxes if board.complete?
+        rescue InvalidBoard, TooManyTries
+          # suppress
         end
       end
 
-      def parameters(n)
+      private def assume_and_solve(board:, boxes:, remain_tries: 30)
+        raise TooManyTries if remain_tries == 0
+
+        new_board = board.dup
+        new_boxes = boxes.dup
+
+        v = new_board.each_values.reject { |v| v.value }.shuffle.min_by { |v| v.candidates.size }
+        new_boxes[v.index] = v.value = v.candidates.sample
+        solver(new_board).solve
+
+        return new_board, new_boxes if new_board.complete?
+
+        if has_no_candidates?(new_board)
+          new_board = board.dup
+          new_boxes = boxes.dup
+          new_v = new_board[v.index]
+          new_v.candidates.delete(v.value)
+          solver(new_board).fill_one_candidate(new_v.index)
+          raise InvalidBoard if has_no_candidates?(new_board)
+        end
+
+        assume_and_solve(board: new_board, boxes: new_boxes, remain_tries: remain_tries - 1)
+      end
+
+      private def solver(board)
+        Namero::Solver.new(board, extensions: [Namero::SolverExtensions::CandidateExistsOnlyOnePlace])
+      end
+
+      private def has_no_candidates?(board)
+        board.each_values.any? { |v| v.candidates.empty? }
+      end
+
+      private def parameters(n)
         initial_filled_counts =
           case n
           when 4
             [*4..6] # is it proper?
           when 9
-            [*20..30]
-          # when 16 # 16x16 is too slow
-          #   [*70..90] # is it proper?
+            [*20..28]
+          when 16 # 16x16 is too slow
+            [*80..90] # is it proper?
           else
             raise UserVisibleError, "not supported N: #{n}"
           end
@@ -68,10 +105,11 @@ module Ruboty
           root_n: Integer.sqrt(n),
           box_count: n * n,
           initial_filled_counts: initial_filled_counts,
+          assume_retry_count: {4 => 0, 9 => 2, 16 => 30},
         }
       end
 
-      def format(boxes, n:, root_n:, **)
+      private def format(boxes, n:, root_n:, **)
         res = ''
         boxes.each_slice(n).with_index do |line, idx|
           line.each_slice(root_n) do |group|
@@ -97,6 +135,13 @@ module Ruboty
           7 => ':seven:',
           8 => ':eight:',
           9 => ':nine:',
+          10 => ':a:',
+          11 => ':b:',
+          12 => ':c:',
+          13 => ':d:',
+          14 => ':e:',
+          15 => ':f:',
+          16 => ':zero:',
           nil => ':transparent2:',
         }[n]
       end
