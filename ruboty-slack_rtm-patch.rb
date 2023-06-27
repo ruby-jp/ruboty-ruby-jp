434b7dc08134fc5577cf03f8038cd1cef631d543
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-06-27 19:20:12 +0900

    Fix pagination of conversations_list

--- ruboty-slack_rtm-patch.rb
+++ ruboty-slack_rtm-patch.rb
@@ -2,10 +2,14 @@ require 'ruboty/slack_rtm'
 
 module Ruboty_Adapters_SlackRTM_MonkeyPatch
   def make_channels_cache
+    @channel_info_caches ||= {}
     # resp = client.channels_list
     # => {"ok"=>false, "error"=>"unknown_method", "req_method"=>"channels.list"}
-    resp = client.conversations_list
-    if resp['ok']
+    next_cursor = nil
+    until next_cursor&.empty?
+      resp = @client.conversations_list(exclude_archived: true, cursor: next_cursor, limit: 300)
+      return unless resp['ok']
+      next_cursor = resp.dig('response_metadata', 'next_cursor')
       resp['channels'].each do |channel|
         @channel_info_caches[channel['id']] = channel
       end
